65e086e1904eca3fedbc4e0f7d7803106f55dbef
Cristina Budinschi <cristina.budinschi@gmail.com>
2020-05-30 15:17:47 +0300

    dot net

---
+++ x_DotNet1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using DotNet1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello World!");
+        //Exercise ex = new Exercise();
+        //ex.Exercise6();
+
+        Homework firstHomework = new Homework();
+        firstHomework.homework6bis();
+    }
+}
