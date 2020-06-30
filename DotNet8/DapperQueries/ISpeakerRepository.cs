82a21294f8c84c71e90bef252c3702f86b172f3f
Cristina Budinschi <cristina.budinschi@gmail.com>
2020-06-30 17:24:31 +0300

    homework

--- DotNet8/DapperQueries/ISpeakerRepository.cs
+++ DotNet8/DapperQueries/ISpeakerRepository.cs
@@ -6,12 +6,12 @@ namespace ConferencePlanner.Data
 {
     public interface ISpeakerRepository
     {
-        Task<IEnumerable<Speaker>> CountSessionPerSpeaker();
+        /*Task<IEnumerable<Speaker>> CountSessionPerSpeaker();
 
         Task<Speaker> Get(int id);
 
         Task<IEnumerable<Session>> GetAllSessions(int id);
 
-        Task<int> Save(Speaker speaker);
+        Task<int> Save(Speaker speaker);*/
     }
 }
